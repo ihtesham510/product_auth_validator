35c1db9b93353025e086926c52a22e6827b2c7ed
ihtesham510 <ihteshamulhaq510@gmail.com>
2025-11-25 17:32:54 +0500

    created project

---
+++ src/routes/__root.tsx
@@ -0,0 +1,29 @@
+import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TanStackDevtools } from '@tanstack/react-devtools'
+
+import Header from '../components/Header'
+
+import ConvexProvider from '../integrations/convex/provider'
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <ConvexProvider>
+        <Header />
+        <Outlet />
+        <TanStackDevtools
+          config={{
+            position: 'bottom-right',
+          }}
+          plugins={[
+            {
+              name: 'Tanstack Router',
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+          ]}
+        />
+      </ConvexProvider>
+    </>
+  ),
+})
